fbe9eec5cf8f21b56d15e839c672c46b48336978
Haruka Ichinose <hic21@uclive.ac.nz>
2022-01-05 11:51:38 +1300

    First commit

---
+++ src/main/java/com/example/formloginjpa/FormLoginJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.formloginjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormLoginJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FormLoginJpaApplication.class, args);
+  }
+
+}
